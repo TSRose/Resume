fb5eddd4f4f411d3dfcc48967ecb8a3444c42068
TSRose <teegs.rose@gmail.com>
2021-10-07 14:37:39 +0000

    Tidy up of code, updated Interests page, map API added

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,25 +1,20 @@
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 12,
+        zoom: 3,
         center: {
-            lat: 52.3667,
-            lng: 4.8945
+            lat: 52.3676,
+            lng: 4.9041
         },
     },
     );
 
     var labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
 
-    var locations = [{
-        lat: 40.785091,
-        lng: 4.968285
-    }, {
-        lat: 41.084045,
-        lng: 4.874256
-    }, {
-        lat: 40.754932,
-        lng: 4.984016
-    }];
+    var locations = [
+        {lat: 37.8136, lng: 144.9631}, 
+        {lat: 52.5200, lng: 13.4050}, 
+        {lat: 52.3676, lng: 4.9041}
+    ];
 
     var markers = locations.map(function (location, i) {
         return new google.maps.Marker({
